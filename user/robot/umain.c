4d157e3b8762ecb49b191cc33762bb1d7e71a8f3
Merritt Boyd <mboyd@mit.edu>
2012-01-14 14:27:25 -0500

    Add VPS target seeking code.

--- user/robot/umain.c
+++ user/robot/umain.c
@@ -4,7 +4,7 @@
 
 extern volatile uint8_t robot_id;
 
-int usetup (void) {
+int usetup(void) {
     robot_id = 7;
     
     platform_init();
@@ -12,15 +12,20 @@ int usetup (void) {
     return 0;
 }
 
-int umain (void) {
+int umain(void) {
     printf("Hello, world!\n");
     
     nav_start();
     
     printf("Nav started, setting coords\n");
     moveToPoint(10, 0, 150);
-    //turnToHeading(45);
     printf("Coords set\n");
+    
+    //while (1) {
+    //    copy_objects();
+    //    moveToPoint(objects[2].x, objects[2].y, 200);
+    //    waitForMovementComplete();
+    //}
     
     return 0;
 }
